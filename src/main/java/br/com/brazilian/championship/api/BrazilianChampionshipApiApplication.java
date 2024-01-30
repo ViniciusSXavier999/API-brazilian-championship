7f9d028b278ed5656c73f22d79127040a1bcd5e9
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-01-30 14:38:40 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/brazilian/championship/api/BrazilianChampionshipApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazilian.championship.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianChampionshipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianChampionshipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brazilian/championship/api/BrazilianChampionshipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazilian.championship.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianChampionshipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
